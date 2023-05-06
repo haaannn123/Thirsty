68413e6be71cf93b712a9fc02fd4d43d6b86f01c
haaannn123 <haaannn97@gmail.com>
2023-05-06 19:24:02 -0400

    search features and styling to navbar

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
-import { login } from "../../store/session";
+import React, { useState, useEffect, useRef } from "react";
+import { login, logout } from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
+import OpenModalButton from "../OpenModalButton";
+import SignupFormModal from "../SignupFormModal";
 import "./LoginForm.css";
 
 function LoginFormModal() {
@@ -9,7 +11,9 @@ function LoginFormModal() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
+  const [showMenu, setShowMenu] = useState(false);
   const { closeModal } = useModal();
+  const ulRef = useRef();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,39 +21,59 @@ function LoginFormModal() {
     if (data) {
       setErrors(data);
     } else {
-        closeModal()
+      closeModal();
     }
   };
 
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+  };
+
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+  const closeMenu = () => setShowMenu(false);
+
   return (
     <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
+      <div className="signin-container">
+        <div className="login">
+          <h1>Sign In</h1>
+          <OpenModalButton buttonText="Sign Up" onItemClick={closeMenu} modalComponent={<SignupFormModal />} />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+          <div className="login-child-container">
+            <label>Email</label>
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <label>Password</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <button type="submit">Log In</button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -2,22 +2,29 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import SearchBar from '../SearchBar';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+		<div className="navbar-container">
+			<div className="navbar-top-row">
+				<div>
+					<NavLink exact to="/" className="home-link">Thirsty</NavLink>
+				</div>
+				{isLoaded && (
+					<div>
+						<ProfileButton user={sessionUser} />
+					</div>
+				)}
+			</div>
+			<div className="navbar-bottom-row">
+				< SearchBar />
+			</div>
+
+		</div>
 	);
 }
 
