da4ae52f910698f945bfc5f0626e2c363e0165a3
Mehedi Meem <waheed816@gmail.com>
2023-05-07 14:56:37 -0400

    search backend now works for multiple terms search

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -8,13 +8,29 @@ search_routes = Blueprint('search', __name__)
 @search_routes.route('/<string:search_terms>')
 def search(search_terms):
 
+    print('INTIAL SEARCH TERMS', search_terms)
 
-    ## the or_() method from SQLAlchemy to combine two filter conditions using the OR operator
-    ## This query creates two filter conditions using the ilike() method to perform a CASE-INSENSITIVE search for search_terms in the name and description fields of the Product table. These conditions are then combined using the or_() method, which results in a query that matches records where either condition is true. Finally, the all() method is called to execute the query and return all matching records.
-    products_search_matches = Product.query.filter(or_(Product.name.ilike(f'%{search_terms}%'), Product.description.ilike(f'%{search_terms}%'))).all()
+    search_terms = search_terms.split()
 
-    ## cast results in a set to eliminate multiple query result from the same item
-    unique_search_results = set(products_search_matches)
+    print("fsdfsdfsdfsfsfs", search_terms)
+
+    search_matched_products =[]
+
+
+    for term in search_terms:
+
+        print('GJGJHGJHGJHGKHJGKJ', term)
+
+        ## the or_() method from SQLAlchemy to combine two filter conditions using the OR operator
+        ## This query creates two filter conditions using the ilike() method to perform a case-INSENSITIVE search for search_terms in the name and description fields of the Product table. These conditions are then combined using the or_() method, which results in a query that matches records where either condition is true. Finally, the all() method is called to execute the query and return all matching records.
+
+        term_matched_products = Product.query.filter(or_(Product.name.ilike(f'%{term}%'), Product.description.ilike(f'%{term}%'))).all()
+
+        search_matched_products.extend(term_matched_products)
+
+
+    ## cast results to a set to eliminate multiple query results of the same product
+    unique_search_results = set(search_matched_products)
 
     search_results_to_dict = [product.to_dict() for product in unique_search_results]
 
