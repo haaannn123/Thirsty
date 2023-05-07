12800b198a39865291f9022120abb3587c517ae3
haaannn123 <haaannn97@gmail.com>
2023-05-07 18:02:30 -0400

    form changes

--- app/forms/product_form.py
+++ app/forms/product_form.py
@@ -8,8 +8,8 @@ from ..api.aws_helpers import ALLOWED_EXTENSIONS
 class ProductForm(FlaskForm):
     # id = IntegerField
     # owner_id = IntegerField('Owner Id', validators=[DataRequired()])
-    name = StringField('Name', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
-    price = DecimalField('Price', validators=[DataRequired()])
-    preview_img = StringField('Preview Image', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), Length(min=3, max=255, message='Name must be between 3 and 255 characters')])
+    description = StringField('Description', validators=[DataRequired(), Length(min=30, max=255, message='Description must be between       30 and 255 characters')])
+    price = DecimalField('Price', places=2, rounding=None, number_format=3, validators=[DataRequired(), NumberRange(min=1)])
+    preview_img = StringField('Preview Image', validators=[DataRequired(), URL(require_tld=True)])
     submit = SubmitField('Submit')
