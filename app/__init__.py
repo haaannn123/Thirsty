227113fbf32429f755d894de327e18782c152930
Waheed <111588666+waheed816@users.noreply.github.com>
2023-05-07 15:14:50 -0400

    Waheed building search feature (#34)
    * search feature backend seems to work
    
    * search backend now works for multiple terms search

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.product_routes import product_routes
+from .api.search_routes import search_routes
+
 from .seeds import seed_commands
 from .config import Config
 
@@ -30,6 +32,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(product_routes, url_prefix='/api/products')
+app.register_blueprint(search_routes, url_prefix='/api/search')
 db.init_app(app)
 Migrate(app, db)
 
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -32,6 +32,7 @@ function App() {
           <Route path="/">
             <GetAllProducts />
           </Route>
+          
         </Switch>
       )}
     </>
