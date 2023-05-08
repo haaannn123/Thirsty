f2292cf2c792b8f80e3cfdb8f71b5cb9379f5e78
Christian Oviedo <108007042+Christian-815@users.noreply.github.com>
2023-05-08 11:14:05 -0700

    All tables done (#43)
    * Tables done excluding images
    
    * Tables done
    
    * Tables
    
    * Merge works

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,8 @@
 from .db import db
 from .user import User
 from .product_model import Product
+from .review_model import Review
+from .shop_model import Shop
+from .shopping_cart_model import Shopping_Cart
+from .images_model import Product_Image
 from .db import environment, SCHEMA
---
+++ app/seeds/shop_seeds.py
@@ -0,0 +1,35 @@
+from app.models import db, Shop, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+def seed_shops():
+    shop1 = Shop(
+        owner_id = 1,
+        product_id = 1,
+        name = 'Horchata Store!'
+    )
+
+    shop2 = Shop(
+        owner_id = 2,
+        product_id = 2,
+        name = 'Thai Tea Store!'
+    )
+
+    shop3 = Shop(
+        owner_id = 3,
+        product_id = 3,
+        name = 'Yerba Mate Store!'
+    )
+
+    all_shops = [shop1, shop2, shop3]
+    add_shops = [db.session.add(shop) for shop in all_shops]
+    db.session.commit()
+
+
+def undo_shops():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.products RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM shops"))
+
+    db.session.commit()
