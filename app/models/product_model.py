88feb8636beec3a45529f7b2d00e5fcdf4b07474
Mehedi Meem <waheed816@gmail.com>
2023-05-10 20:27:34 -0400

    changed product model to cascade delete associated reviews when product is deleted

--- app/models/product_model.py
+++ app/models/product_model.py
@@ -19,7 +19,7 @@ class Product(db.Model):
 
     users = db.relationship("User", back_populates="products")
     shop_product = db.relationship("Shop", back_populates="product_shop")
-    review_product = db.relationship("Review", back_populates="product_review")
+    review_product = db.relationship("Review", cascade="all, delete-orphan", back_populates="product_review")
     cart_product = db.relationship("Shopping_Cart", back_populates="product_cart")
     image_product = db.relationship("Product_Image", back_populates="product_image")
 
