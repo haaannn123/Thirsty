a93fdae21d401c030b00731055cce8c555df3ccc
Christian Oviedo <Chris_ovi1@hotmail.com>
2023-05-10 17:14:44 -0700

    Populated update review

--- react-app/src/components/SingleProduct/index.js
+++ react-app/src/components/SingleProduct/index.js
@@ -6,6 +6,8 @@ import { thunkGetProductReviews } from '../../store/reviews'
 import OpenModalButton from "../OpenModalButton";
 import CreateNewReview from '../CreateNewReview'
 import AddToCart from '../AddToCart'
+import UpdateReview from '../UpdateReview'
+import { thunkGetUserReviews } from '../../store/reviews'
 
 // import { CreateNewReview}
 
@@ -17,14 +19,30 @@ const GetSingleProduct = () => {
     const product = useSelector((state) => state.products.allProducts[product_id])
     const reviews = Object.values(useSelector(state => state.productReviews.productReviews))
     const new_review = useSelector(state => state.productReviews.newReview)
+    const user = useSelector(state => state.session.user)
     console.log('-----SINGLE PRODUCT IN COMPONENT----', product, reviews, new_review)
 
     useEffect(() => {
         dispatch(fetchProduct(product_id))
         dispatch(thunkGetProductReviews(product_id))
+        dispatch(thunkGetUserReviews(user.id))
     }, [dispatch, product_id, new_review])
 
     if (!product || !reviews) return null
+
+    const userLoggedIn = (review) => {
+        if (review.user_id === user.id) {
+            return (
+                <div>
+                    <OpenModalButton
+                        buttonText="Update your review"
+                        modalComponent={<UpdateReview product={product_id} />}
+                    />
+                </div>
+            )
+        }
+    }
+
     return (
         <div>
             <div>
@@ -37,12 +55,10 @@ const GetSingleProduct = () => {
             </div>
             <div>
                 <h2>Reviews</h2>
-                <button>
-                    <OpenModalButton
-                        buttonText="Leave a Review"
-                        modalComponent={<CreateNewReview product={product_id}/>}
-                    />
-                </button>
+                <OpenModalButton
+                    buttonText="Leave a Review"
+                    modalComponent={<CreateNewReview product={product_id} />}
+                />
                 {reviews.map(review =>
                     {
                     return (
@@ -55,6 +71,7 @@ const GetSingleProduct = () => {
                                 <div>{review.User_info.username}</div>
                                 <div>{review.created_at}</div>
                             </div>
+                            {userLoggedIn(review)}
                         </div>
                     )
                     })}
