c5e2d65e146c4c8d7c28a4320f1c74d63f2c763c
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-05-11 14:32:44 -0700

    small changes for reviews and product details

--- react-app/src/components/SingleProduct/index.js
+++ react-app/src/components/SingleProduct/index.js
@@ -10,17 +10,18 @@ import UpdateReview from '../UpdateReview'
 import { thunkGetUserReviews } from '../../store/reviews'
 import DeleteReview from '../DeleteReviewModal'
 
+
 // import { CreateNewReview}
 
 const GetSingleProduct = () => {
-    const {product_id}  = useParams()
+    const { product_id } = useParams()
     const dispatch = useDispatch()
 
     const product = useSelector((state) => state.products.allProducts[product_id])
     const reviews = Object.values(useSelector(state => state.productReviews.productReviews))
     const new_review = useSelector(state => state.productReviews.newReview)
     const user = useSelector(state => state.session.user)
-    // console.log('-----SINGLE PRODUCT IN COMPONENT----', product, reviews, new_review)
+    console.log('-----SINGLE PRODUCT IN COMPONENT----', reviews)
 
     useEffect(() => {
         dispatch(fetchProduct(product_id))
@@ -32,7 +33,7 @@ const GetSingleProduct = () => {
 
     const userLoggedIn = (review, user_id) => {
         if (review.user_id === user_id) {
-        dispatch(thunkGetUserReviews(user_id))
+            dispatch(thunkGetUserReviews(user_id))
             return (
                 <div>
                     <OpenModalButton
@@ -48,24 +49,41 @@ const GetSingleProduct = () => {
         }
     }
 
+    function formatDate(created_at) {
+        const dateObj = new Date(created_at);
+        const options = { month: "long", day: "numeric", year: "numeric" };
+        return dateObj.toLocaleDateString('en-US', options);
+    }
+
+    function averageRating(reviews) {
+        let sum = 0
+        reviews.forEach((review) =>
+            sum += review.rating
+        )
+        return (sum / reviews.length).toFixed(2)
+    }
+
     return (
         <div>
             <div>
                 <h1>{product.name}</h1>
                 <img src={product.preview_img} alt="this is a drink!" />
-                <h2>{product.price}</h2>
+                <h2>${product.price}</h2>
                 <text>{product.description}</text>
                 <button>Buy it now</button>
                 <AddToCart />
             </div>
             <div>
-                <h2>Reviews</h2>
+                <h2>{averageRating(reviews)}
+                    {reviews.length ? <div>·</div> : null}
+                    {reviews.length && reviews.length === 1 ? reviews.length + ' review' : ''}
+                    {reviews.length && reviews.length !== 1 ? reviews.length + ' reviews' : ''}
+                </h2>
                 <OpenModalButton
                     buttonText="Leave a Review"
                     modalComponent={<CreateNewReview product={product_id} />}
                 />
-                {reviews.map(review =>
-                    {
+                {reviews.map(review => {
                     return (
                         <div key={review.id}>
                             <div>
@@ -74,21 +92,17 @@ const GetSingleProduct = () => {
                             </div>
                             <div>
                                 <div>{review.User_info.username}</div>
-                                <div>{review.created_at}</div>
+                                <div>{formatDate(review.created_at)}</div>
                             </div>
-                            {user ?
-                                <div>
-                                    {userLoggedIn(review, user.id)}
-                                </div>
-                                :
-                                null
-                            }
+                            {user ? (
+                                <div>{userLoggedIn(review, user.id)}</div>
+                            ) : null}
                         </div>
-                    )
-                    })}
+                    );
+                })}
             </div>
         </div>
-    )
+    );
 }
 
 export default GetSingleProduct
