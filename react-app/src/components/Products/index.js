837bd6c9d195f0c90b00581e442450de3af3deb9
haaannn123 <haaannn97@gmail.com>
2023-05-14 11:51:54 -0400

    navigation

--- react-app/src/components/Products/index.js
+++ react-app/src/components/Products/index.js
@@ -8,6 +8,8 @@ import "./products.css";
 const GetAllProducts = () => {
   const dispatch = useDispatch();
   const allProducts = Object.values(useSelector((state) => state.products.allProducts));
+  const sessionUser = useSelector((state) => state.session.user);
+  console.log('SESSIONUSERHERE!:', sessionUser)
 
   useEffect(() => {
     dispatch(thunkGetAllProducts());
@@ -17,24 +19,33 @@ const GetAllProducts = () => {
     return <i className="fa-solid fa-truck-ramp-box spot-info-loading">LOADING...</i>;
   }
 
+  const welcome = () => {
+    if (sessionUser){
+      return `Welcome back, ${sessionUser.username}`
+    } else {
+      return 'Welcome to Thristy!'
+    }
+  }
+
   return (
-    <div className="all-products-container">
-      {allProducts.map((product) => {
-        return (
-          <div key={product.id} className="all-products-card">
-            <NavLink to={`/products/${product.id}`}>
-              <img src={product.preview_img} alt={`${product.name}'s unavaiable`} className="all-products-image"></img>
-              <div class="all-products-price-container">
-                <div className="all-products-price">${`${product.price.toFixed(2)}`}</div>
-              </div>
-            </NavLink>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="welcome-back">{welcome()}</div>
+      <div className="all-products-container">
+        {allProducts.map((product) => {
+          return (
+            <div key={product.id} className="all-products-card">
+              <NavLink to={`/products/${product.id}`}>
+                <img src={product.preview_img} alt={`${product.name}'s unavaiable`} className="all-products-image"></img>
+                <div class="all-products-price-container">
+                  <div className="all-products-price">${`${product.price.toFixed(2)}`}</div>
+                </div>
+              </NavLink>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
-
-
 
 export default GetAllProducts;
