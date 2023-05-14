210fb22cfc693ddefc60a33aa04ab702111b4d23
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-05-14 15:49:26 -0700

    footer completed

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,8 +1,83 @@
 import React from "react";
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <h1>FOOTER</h1>
+        <div className="footer-container">
+            <h2 className="footer-header">Meet the Team</h2>
+            <div className="all-group-container">
+                <div className="person-container">
+                    <h4 className="single-name">Ash Iranfar</h4>
+                    <div className="links-wrapper">
+                        <a href="https://www.linkedin.com/in/ashkaun-iranfar-608387220/" className="linkedin-link">
+                            <div className="linked-in-wrapper">
+                                <div className="linkedin-github-text">LinkedIn</div>
+                                <i class="fa-brands fa-linkedin"></i>
+                            </div>
+                        </a>
+                        <a href="https://github.com/AIranfar" className="github-link">
+                            <div className="github-wrapper">
+                                <div className="linkedin-github-text">Github</div>
+                                <i class="fa-brands fa-github"></i>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div className="person-container">
+                    <h4 className="single-name">Christian Oviedo</h4>
+                    <div className="links-wrapper">
+
+                        <a href="https://www.linkedin.com/in/christian-oviedo-6a1586242/" className="linkedin-link">
+                            <div className="linked-in-wrapper">
+                                <div className="linkedin-github-text">LinkedIn</div>
+                                <i class="fa-brands fa-linkedin"></i>
+                            </div>
+                        </a>
+                        <a href="https://github.com/Christian-815" className="github-link">
+                            <div className="github-wrapper">
+                                <div className="linkedin-github-text">Github</div>
+                                <i class="fa-brands fa-github"></i>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div className="person-container">
+                    <h4 className="single-name">Han Nguyen</h4>
+                    <div className="links-wrapper">
+
+                        <a href="https://www.linkedin.com/in/christian-oviedo-6a1586242/" className="linkedin-link">
+                            <div className="linked-in-wrapper">
+                                <div className="linkedin-github-text">LinkedIn</div>
+                                <i class="fa-brands fa-linkedin"></i>
+                            </div>
+                        </a>
+                        <a href="https://github.com/haaannn123" className="github-link">
+                            <div className="github-wrapper">
+                                <div className="linkedin-github-text">Github</div>
+                                <i class="fa-brands fa-github"></i>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div className="person-container">
+                    <h4 className="single-name">Waheed Meem</h4>
+                    <div className="links-wrapper">
+                        <a href="https://www.linkedin.com/in/mehedi-waheed-meem-405483257/" className="linkedin-link">
+                            <div className="linked-in-wrapper">
+                                <div className="linkedin-github-text">LinkedIn</div>
+                                <i class="fa-brands fa-linkedin"></i>
+                            </div>
+                        </a>
+                        <a href="https://github.com/waheed816" className="github-link">
+                            <div className="github-wrapper">
+                                <div className="linkedin-github-text">Github</div>
+                                <i class="fa-brands fa-github"></i>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
