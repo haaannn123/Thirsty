b1fe7c4b5cfd3a6e6df0d56797a9b6fc0d92ba3a
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-05-13 20:23:56 -0700

    edit product form styled

--- react-app/src/components/EditProduct/index.js
+++ react-app/src/components/EditProduct/index.js
@@ -4,9 +4,10 @@ import { useState, useEffect } from 'react';
 import { editProduct } from '../../store/products';
 import { useParams } from 'react-router-dom';
 import { fetchProduct } from '../../store/products';
+import './EditProduct.css';
 
 const EditProduct = () => {
-    const {productId} = useParams();
+    const { productId } = useParams();
     const dispatch = useDispatch();
     const history = useHistory();
 
@@ -18,7 +19,6 @@ const EditProduct = () => {
     const [price, setPrice] = useState(product.price);
     // const [preview_img, setPreview_img] = useState(product.preview_img);
     const [errors, setErrors] = useState('');
-
 
     useEffect(() => {
         if (product) {
@@ -32,7 +32,7 @@ const EditProduct = () => {
         dispatch(fetchProduct(productId))
     }, [dispatch, productId])
 
-    if (!product){
+    if (!product) {
         return (<div>Loading</div>)
     }
 
@@ -50,7 +50,7 @@ const EditProduct = () => {
         // if (!preview_img || preview_img === '') allErrors.preview_img = 'Preview image is required'
         // if (!preview_img.endsWith('.png') && !preview_img.endsWith('.jpg') && !preview_img.endsWith('.jpeg')) allErrors.preview_img = 'Image URL must end in .png, .jpg, or .jpeg'
 
-        console.log("PRICE------------------", typeof(price))
+        console.log("PRICE------------------", typeof (price))
 
         if (Object.keys(allErrors).length) {
             return setErrors(allErrors)
@@ -70,55 +70,61 @@ const EditProduct = () => {
         await dispatch(fetchProduct(updatedProduct.id))
 
         // if (updatedProduct) {
-            history.push(`/products/${updatedProduct.id}`)
+        history.push(`/products/${updatedProduct.id}`)
         //     return
         // }
     }
 
     return (
-        <div>
-            <h2>Update Product</h2>
-            <form method='POST' encType="multipart/form-data" onSubmit={handleSubmit}>
-                <label>Name</label>
-                {errors.name ? <p>{errors.name}</p> : null}
-                <input
-                    type='text'
-
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}
-                    placeholder='Name'
-                    name='name'
-                />
-                <label>Description</label>
-                {errors.description ? <p>{errors.description}</p> : null}
-                <textarea
-                    type='textbox'
-                    onChange={(e) => setDescription(e.target.value)}
-                    value={description}
-                    placeholder='Please write at least 30 characters'
-                    name='description'
-                    rows='7'
-                />
-                <label>Price</label>
-                {errors.price ? <p>{errors.price}</p> : null}
-                <input
-                    type='number'
-                    onChange={(e) => setPrice(e.target.value)}
-                    value={price}
-                    placeholder='Price'
-                    name='price'
-                />
-                {/* <label>Preview Image</label>
+        <div className='edit-product-container'>
+            <h1 className='edit-product-header'>Update Your Product</h1>
+            <div className='edit-product-photo-form'>
+                <img className='edit-product-photo' src={product.preview_img} alt="this is a drink!" />
+                <form className='edit-product-form-container' method='POST' encType="multipart/form-data" onSubmit={handleSubmit}>
+                    <label className='edit-product-label'>Name</label>
+                    {errors.name ? <p>{errors.name}</p> : null}
+                    <input
+                        type='text'
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        placeholder='Name'
+                        name='name'
+                    />
+                    <label className='edit-product-label'>Description</label>
+                    {errors.description ? <p>{errors.description}</p> : null}
+                    <textarea
+                        className='edit-product-description'
+                        type='textbox'
+                        onChange={(e) => setDescription(e.target.value)}
+                        value={description}
+                        placeholder='Please write at least 30 characters'
+                        name='description'
+                        rows='7'
+                    />
+                    <label className='edit-product-label'>Price($)</label>
+                    {errors.price ? <p>{errors.price}</p> : null}
+                    <input
+                        className='edit-product-price'
+                        type='number'
+                        onChange={(e) => setPrice(e.target.value)}
+                        value={price}
+                        placeholder='Price'
+                        name='price'
+                    />
+                    {/* <label>Preview Image</label>
                 {errors.preview_img ? <p>{errors.preview_img}</p> : null}
                 <input
-                    type='file'
-                    accept=".jpg, .jpeg, .png"
-                    onChange={(e) => setPreview_img(e.target.files[0])}
-                    placeholder='Preview Image'
-                    name='preview_img'
-                /> */}
-                <button type='submit'>Create a New Product</button>
-            </form>
+                type='file'
+                accept=".jpg, .jpeg, .png"
+                onChange={(e) => setPreview_img(e.target.files[0])}
+                placeholder='Preview Image'
+                name='preview_img'
+            /> */}
+                    <div className='edit-submit-button-container'>
+                        <button className='edit-product-submit-button' type='submit'>Create a New Product</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
