da3881384a06748dc8916abab3894a2e70d46eff
Christian Oviedo <Chris_ovi1@hotmail.com>
2023-05-14 00:21:23 -0700

    css and some singleproduct changes

--- react-app/src/components/SingleProduct/index.js
+++ react-app/src/components/SingleProduct/index.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchProduct } from "../../store/products";
 import { thunkGetProductReviews } from "../../store/reviews";
 import OpenModalButton from "../OpenModalButton";
@@ -14,6 +14,8 @@ import "./SingleProduct.css";
 const GetSingleProduct = () => {
   const { product_id } = useParams();
   const dispatch = useDispatch();
+  const [count, setCount] = useState(1);
+
 
   const product = useSelector((state) => state.products.allProducts[product_id]);
   const reviews = Object.values(useSelector((state) => state.productReviews.productReviews));
@@ -54,7 +56,7 @@ const GetSingleProduct = () => {
     let sum = 0;
     if (reviews.length > 0) {
       reviews.forEach((review) => (sum += review.rating));
-      return (sum / reviews.length).toFixed(2);
+      return "Average rating " + (sum / reviews.length);
     }
     return "No Reviews";
   }
@@ -85,44 +87,107 @@ const GetSingleProduct = () => {
     }
   };
 
+  const options = [];
+  for (let i = 1; i <= 50; i++) {
+    options.push(
+      <option key={i} value={i}>
+        {i}
+      </option>
+    );
+  }
+
 
 
   return (
-    <div>
-      <div>
-        <h1>{product.name}</h1>
-        <img src={product.preview_img} alt="this is a drink!" />
-        <h2>${product.price}</h2>
-        <text>{product.description}</text>
-        <button onClick={handleClick}>Buy it now</button>
-        <AddToCart />
+    <div className="c-single-product">
+
+      <div className="c-product-info-block">
+
+        <div className="c-product-info-left">
+          <img src={product.preview_img} alt="this is a drink!" className="c-product-image" />
+        </div>
+
+        <div className="c-product-info-right">
+          <div>
+            <h1>${product.price}</h1>
+            <h1>{product.name}</h1>
+            <div className="c-product-owner">
+              Sold by {product.owner_info}
+            </div>
+          </div>
+
+          <div className="c-product-quantity-block">
+            <h2>How thirsty are you?</h2>
+            <div>
+              <select value={count} onChange={((e) => setCount(e.target.value))} className="c-product-quantity">
+                {options}
+              </select>
+            </div>
+          </div>
+
+          <div className="c-product-purchase">
+            <div>
+              <button onClick={handleClick} className="c-product-buynow">Buy it now</button>
+            </div>
+            <div>
+              <AddToCart quantity={count}/>
+            </div>
+          </div>
+        </div>
+
       </div>
+
       <div>
         <h2>
-          {averageRating(reviews)}
-          <i class="fa-solid fa-wine-glass"></i>
-          {reviews.length ? <div>·</div> : null}
-          {reviews.length && reviews.length === 1 ? reviews.length + " review" : ""}
-          {reviews.length && reviews.length !== 1 ? reviews.length + " reviews" : ""}
-        </h2>
-        {renderLeaveReviewButton()}
-        {reviews.map((review) => {
-          return (
-            <div key={review.id} className="review-body">
-              <div>
-                <div>{renderRatings(review)}</div>
-
-                <div>{review.review}</div>
-              </div>
-              <div>
-                <div>{review.User_info.username}</div>
-                <div>{formatDate(review.created_at)}</div>
-              </div>
-              {user ? <div>{userLoggedIn(review, user.id)}</div> : null}
+          <div className="c-product-avgrating-block">
+            <div>
+              {averageRating(reviews)}
             </div>
-          );
-        })}
+            <div>
+              <i className="fa-solid fa-wine-glass c-product-avgrating"></i>
+            </div>
+          </div>
+        </h2>
+        <h2 className="c-product-description-header">Drink Description</h2>
+        <div className="c-product-description">
+          {product.description}
+        </div>
       </div>
+
+      <div className="c-product-reviews-block">
+
+        <div className="c-product-reviews-header">
+          <h2>
+            Reviews
+          </h2>
+          {reviews.length ? <h2>·</h2> : null}
+          {reviews.length ? <h2>({reviews.length})</h2> : null}
+          <div>
+            {renderLeaveReviewButton()}
+          </div>
+        </div>
+
+        <div className="c-product-reviews-list">
+          {reviews.map((review) => {
+            return (
+              <div key={review.id} className="review-body">
+                <div>
+                  <div>{renderRatings(review)}</div>
+
+                  <div>{review.review}</div>
+                </div>
+                <div>
+                  <div>{review.User_info.username}</div>
+                  <div>{formatDate(review.created_at)}</div>
+                </div>
+                {user ? <div>{userLoggedIn(review, user.id)}</div> : null}
+              </div>
+            );
+          })}
+        </div>
+
+      </div>
+
     </div>
   );
 };
