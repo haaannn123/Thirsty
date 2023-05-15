e847ec856fedd4750e6d3042bf12b739780af21b
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-05-14 17:24:45 -0700

    fixed footer/update review size/new review size

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -9,13 +9,13 @@ const Footer = () => {
                 <div className="person-container">
                     <h4 className="single-name">Ash Iranfar</h4>
                     <div className="links-wrapper">
-                        <a href="https://www.linkedin.com/in/ashkaun-iranfar-608387220/" className="linkedin-link">
+                        <a href="https://www.linkedin.com/in/ashkaun-iranfar-608387220/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
                             <div className="linked-in-wrapper">
                                 <div className="linkedin-github-text">LinkedIn</div>
                                 <i class="fa-brands fa-linkedin"></i>
                             </div>
                         </a>
-                        <a href="https://github.com/AIranfar" className="github-link">
+                        <a href="https://github.com/AIranfar" target="_blank" rel="noopener noreferrer" className='linkedin-link'>
                             <div className="github-wrapper">
                                 <div className="linkedin-github-text">Github</div>
                                 <i class="fa-brands fa-github"></i>
@@ -27,13 +27,13 @@ const Footer = () => {
                     <h4 className="single-name">Christian Oviedo</h4>
                     <div className="links-wrapper">
 
-                        <a href="https://www.linkedin.com/in/christian-oviedo-6a1586242/" className="linkedin-link">
+                        <a href="https://www.linkedin.com/in/christian-oviedo-6a1586242/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
                             <div className="linked-in-wrapper">
                                 <div className="linkedin-github-text">LinkedIn</div>
                                 <i class="fa-brands fa-linkedin"></i>
                             </div>
                         </a>
-                        <a href="https://github.com/Christian-815" className="github-link">
+                        <a href="https://github.com/Christian-815" target="_blank" rel="noopener noreferrer" className="github-link">
                             <div className="github-wrapper">
                                 <div className="linkedin-github-text">Github</div>
                                 <i class="fa-brands fa-github"></i>
@@ -45,13 +45,13 @@ const Footer = () => {
                     <h4 className="single-name">Han Nguyen</h4>
                     <div className="links-wrapper">
 
-                        <a href="https://www.linkedin.com/in/christian-oviedo-6a1586242/" className="linkedin-link">
+                        <a href="https://www.linkedin.com/in/christian-oviedo-6a1586242/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
                             <div className="linked-in-wrapper">
                                 <div className="linkedin-github-text">LinkedIn</div>
                                 <i class="fa-brands fa-linkedin"></i>
                             </div>
                         </a>
-                        <a href="https://github.com/haaannn123" className="github-link">
+                        <a href="https://github.com/haaannn123" target="_blank" rel="noopener noreferrer" className="github-link">
                             <div className="github-wrapper">
                                 <div className="linkedin-github-text">Github</div>
                                 <i class="fa-brands fa-github"></i>
@@ -62,13 +62,13 @@ const Footer = () => {
                 <div className="person-container">
                     <h4 className="single-name">Waheed Meem</h4>
                     <div className="links-wrapper">
-                        <a href="https://www.linkedin.com/in/mehedi-waheed-meem-405483257/" className="linkedin-link">
+                        <a href="https://www.linkedin.com/in/mehedi-waheed-meem-405483257/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
                             <div className="linked-in-wrapper">
                                 <div className="linkedin-github-text">LinkedIn</div>
                                 <i class="fa-brands fa-linkedin"></i>
                             </div>
                         </a>
-                        <a href="https://github.com/waheed816" className="github-link">
+                        <a href="https://github.com/waheed816" target="_blank" rel="noopener noreferrer" className="github-link">
                             <div className="github-wrapper">
                                 <div className="linkedin-github-text">Github</div>
                                 <i class="fa-brands fa-github"></i>
